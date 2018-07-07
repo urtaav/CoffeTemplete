e1b4b0870e41da33ecc7f9c57f43ee471340003b
Uriel1020 <urieltapiaavalos@gmail.com>
2018-07-07 15:41:15 -0500

    Primer Commit
    primer commit

---
+++ CoffeTemplete/js/slider.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var variableWidth = document.querySelector('.js_variablewlidth');
+
+    lory(variableWidth, {
+        rewind: true
+    });
+});
